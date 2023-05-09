fe384483ef2a768f8f2bd52ba5595c02dcb9fca7
Maria Dikul <dikul.m@mail.ru>
2023-05-09 16:27:32 +0300

    Stub and tests for add

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,31 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.PhoneBook;
+
+import java.util.*;
+
 public class PhoneBookTest {
+    PhoneBook book = new PhoneBook();
+
+    @Test
+    public void testAdd() {
+        // given:
+        Map<String, String> expected = new HashMap<>();
+        expected.put("cont1", "111");
+        expected.put("cont2", "222");
+        expected.put("cont3", "333");
+        int expectedSize = expected.size();
+
+
+
+        // when:
+        book.add("cont1", "111");
+        book.add("cont1", "111");
+        book.add("cont2", "222");
+        int result = book.add("cont3", "333");
+
+        // then:
+        Assertions.assertEquals(expectedSize, result);
+
+    }
 }

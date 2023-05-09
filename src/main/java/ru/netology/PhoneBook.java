e5e7c683cfe426029acf56c742b6077c10e50f79
Maria Dikul <dikul.m@mail.ru>
2023-05-09 16:53:12 +0300

    add implemented

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -1,7 +1,14 @@
 package ru.netology;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class PhoneBook {
+
+    Map<String, String> book = new HashMap<>();
     public int add(String name, String number) {
-        return 0;
+        book.put(name, number);
+        return book.size();
     }
 }
